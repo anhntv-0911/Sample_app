b1a211c6c87fe70e1f4f9d58838015f825832c11
Van Anh <nguyen.thi.van.anh@sun-asterisk.com>
2019-07-03 14:42:27 +0700

    finish chapter6

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  before_save { email.downcase! }
+  validates :name, presence: true, length: { maximum: Settings.validate.maxName }
+  VALID_EMAIL_REGEX = Settings.validate.validEmail
+  validates :email, presence: true, length: { maximum: Settings.validate.maxEmail }, format: {with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+  validates :password, presence: true, length: { minimun: Settings.validate.minPass }
+
+  has_secure_password
+end
