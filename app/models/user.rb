988a6645cd45051f471a49878812eb9dd30791a9
anhntv-0911 <nguyen.thi.van.anh@sun-asterisk.com>
2019-07-05 15:27:39 +0700

    Finish chapter7

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   validates :name, presence: true, length: { maximum: Settings.validate.maxName }
   VALID_EMAIL_REGEX = Settings.validate.validEmail
   validates :email, presence: true, length: { maximum: Settings.validate.maxEmail }, format: {with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
-  validates :password, presence: true, length: { minimun: Settings.validate.minPass }
+  validates :password, presence: true, length: { minimum: Settings.validate.minPass }
 
   has_secure_password
 end
